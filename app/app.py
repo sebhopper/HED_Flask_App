21f15ed577327a70039c55fa0f5d5e2f32b20d6c
Seb <seb@admins-air.home>
2025-05-17 21:11:18 +0100

    little_updates

--- app/app.py
+++ app/app.py
@@ -1,6 +1,10 @@
-from app import create_app
+from flask import Flask, render_template
 
-app = create_app()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
