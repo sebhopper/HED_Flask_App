4a04c4279e08325d9f887d05fcc8e84d1c953999
Seb <seb@admins-air.home>
2025-05-18 15:14:20 +0100

    some stuff

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=int(os.getenv("PORT", default=5000)), host="0.0.0.0")
