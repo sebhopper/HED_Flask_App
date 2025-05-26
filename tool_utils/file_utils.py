d7309a4a12dfaf2105d0bbab7a431225ca4ceda0
Seb <seb@admins-air.home>
2025-05-26 20:15:45 +0100

    updates to file utils

--- tool_utils/file_utils.py
+++ tool_utils/file_utils.py
@@ -5,6 +5,3 @@ ALLOWED_EXTENSIONS = {'csv', 'txt'}
 def allowed_file(filename):
     """Check if the uploaded file has an allowed extension."""
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-def write_result_file(data):
-    """Write out a text file for users to download"""
